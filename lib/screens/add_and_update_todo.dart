a8757e98967c1090551de147a1174f8f88e77b19
suupss <demiandsupriya@gmail.com>
2024-09-16 19:48:58 +0545

    feat: form disappear upon tapping screen

--- lib/screens/add_and_update_todo.dart
+++ lib/screens/add_and_update_todo.dart
@@ -1,31 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/Widgets/switch_button.dart';
 
 class AddAndUpdateTodo extends StatelessWidget {
   const AddAndUpdateTodo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.secondary,
-      appBar: AppBar(
-        title: const Text('Add Todo'),
-        centerTitle: true,
-       
-      ),
-      body: const Center(
-        child: SizedBox(
-          height: 400,
-          width: 350,
-          child: Card(
-          elevation: 8,
-            child:  Column(
-          children: [
-            TextField(
-      
 
-            ),
-            TextField()
-          ],
+    return GestureDetector(
+      onTap: FocusScope.of(context).unfocus,
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        appBar: AppBar(
+          title: const Text('Add Todo'),
+          centerTitle: true,
+         
+        ),
+        body:  Center(
+          child: SizedBox(
+            height: 400,
+            width: 350,
+            child: Card(
+            elevation: 8,
+              child:  Column(
+            children: [
+              const TextField(
+        decoration: InputDecoration(
+          labelText: 'Title',
+          labelStyle: TextStyle(color:Color.fromARGB(255, 147, 145, 145) )
+          
+        ),
+      
+              ),
+             const  TextField(
+                
+                      decoration: InputDecoration(
+          labelText: 'Description',
+          labelStyle: TextStyle(color: Color.fromARGB(255, 147, 145, 145),),
+      
+        ),
+        maxLines: null,
+              ),
+              Row(
+                children: [
+                  Text('Complete' , style: Theme.of(context).textTheme.titleMedium,), const Spacer(), const SwitchButton()
+                ],
+              )
+            ],
+
+              ),
             ),
           ),
         ),
