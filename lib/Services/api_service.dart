2fbf35140cc703e3794cc31bfad35d93f9b37060
suupss <demiandsupriya@gmail.com>
2024-09-30 18:40:09 +0545

    chore: remove spaces

--- lib/Services/api_service.dart
+++ lib/Services/api_service.dart
@@ -2,12 +2,14 @@ import 'package:todo_app/Models/get_todos.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:todo_app/Models/todo_model.dart';
 class ApiService {
+  final String url = 'https://api.nstack.in/v1/todos';
 
   //get todos
   Future<GetTodos> fetchData() async{
     try{
-final response = await  http.get(Uri.parse('https://api.nstack.in/v1/todos'));
+final response = await  http.get(Uri.parse(url));
 print(response.body);
 
 
@@ -32,14 +34,14 @@ else {
 
   }
 
-  Future<GetTodos> postTodo( String title , String description, bool is_completed) async {
+  Future<TodoModel> postTodo( String title , String description, bool isCompleted) async {
     try{
-      final response = await http.post(Uri.parse('https://api.nstack.in/v1/todos'),
+      final response = await http.post(Uri.parse(url),
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode( {
   "title": title,
   "description": description,
-  "is_completed": is_completed,
+  "is_completed": isCompleted,
 })
       );
 
@@ -47,7 +49,7 @@ else {
       {
         final data = jsonDecode(response.body);
         print(data);
-        return GetTodos.fromJson(data);
+        return TodoModel.fromJson(data);
       }
       else{
         throw 'No value from API';
@@ -57,6 +59,62 @@ else {
     {
       throw 'An error occured';
     }
+  }
+
+//Udate the todos
+
+  Future<TodoModel> updateTodo( String title , String description, bool isCompleted, String id) async {
+    try{
+      final response = await http.put(Uri.parse('$url/$id'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode( {
+  "title": title,
+  "description": description,
+  "is_completed": isCompleted,
+})
+      );
+
+      if(response.statusCode == 201)
+      {
+        final data = jsonDecode(response.body);
+        print(data);
+        return TodoModel.fromJson(data);
+      }
+      else{
+        throw 'No value from API';
+      }
+    }
+    catch(e)
+    {
+      throw 'An error occured';
+    }
+  }
+  //Detele Todos
+  Future<GetTodos> deleteTodo(String id) async{
+    try{
+final response = await  http.delete(Uri.parse('$url/$id'));
+print(response.body);
+
+
+if(response.statusCode == 200)
+{ 
+  
+
+  final data = jsonDecode(response.body);
+  
+  return GetTodos.fromJson(data);
+}
+else {
+  throw 'No response from API';
+}
+
+    }
+    catch(e)
+    {
+      debugPrint('Error: $e');
+      throw'An exception occured $e';
+    }
+
   }
  
 }
