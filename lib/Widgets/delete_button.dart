c500956bc2a153a25fe78e845ce34dbfe73d1ec4
suupss <demiandsupriya@gmail.com>
2024-09-30 18:41:54 +0545

    refactor: move delete button out of screen

--- lib/Widgets/delete_button.dart
+++ lib/Widgets/delete_button.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/Services/api_service.dart';
+import 'package:todo_app/screens/todo_list_screen.dart';
+import 'package:todo_app/utils/snackbar.dart';
 
 class DeleteButton extends StatefulWidget {
-  const DeleteButton({super.key});
+  final String id;
+  const DeleteButton({super.key,required this.id});
 
   @override
   State<DeleteButton> createState() => _DeleteButtonState();
 }
 
 class _DeleteButtonState extends State<DeleteButton> {
+  void onDeleteButtonPress()
+  {
+ApiService().deleteTodo(widget.id).then((value){
+Navigator.pushReplacement(context, (MaterialPageRoute(builder: (context)=> TodoListScreen() )));
+snackbar(context, 'Deleted successfully');
+});
+
+
+
+  }
   @override
   Widget build(BuildContext context) {
-    return IconButton(onPressed:(){}
+    return IconButton(onPressed: onDeleteButtonPress
                   
                   
                    
