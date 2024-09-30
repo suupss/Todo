be758d47e4ab57cf944675332bacfc424e71b3e1
suupss <demiandsupriya@gmail.com>
2024-09-30 17:10:31 +0545

    feat: add delete button

---
+++ lib/Widgets/delete_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class DeleteButton extends StatefulWidget {
+  const DeleteButton({super.key});
+
+  @override
+  State<DeleteButton> createState() => _DeleteButtonState();
+}
+
+class _DeleteButtonState extends State<DeleteButton> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(onPressed:(){}
+                  
+                  
+                   
+                    
+                
+                 
+           
+                 
+                , icon: const Icon(Icons.delete_outline, color: Color.fromARGB(255, 214, 42, 29),));
+  }
+}
